07c9010037120e1f225b98df7b2f0c59674a4417
isabelPatulski <82146353+isabelPatulski@users.noreply.github.com>
2021-04-08 19:29:31 +0200

    Add files via upload

--- src/Bibliotek.java
+++ src/Bibliotek.java
@@ -1,38 +1,66 @@
 import java.util.ArrayList;
+import java.util.Scanner;
 
+//Exercise
+//Skriv en klasse kaldet Bibliotek, der indeholder en liste af Bog objekter.
+
+//indsæt tre forskellige bøger i biblioteket
+
+//Skriv en metode i Bibliotek klasse, som tager et Bog objekt som parameter og returner true,
+//hvis listen indeholder et Bog objekt med samme ISBN*/
 public class Bibliotek {
-    /*Exercise
-    Skriv en klasse kaldet Bibliotek, der indeholder en liste af Bog objekter.
+    ArrayList<Bibliotek> bookList;
+    String name;
+    String ISBN;
 
-    indsæt tre forskellige bøger i biblioteket
-
-    Skriv en metode i Bibliotek klasse, som tager et Bog objekt som parameter og returnere true,
-    hvis listen indeholder et Bog objekt med samme ISBN*/
 
     public static void main(String[] args) {
+        Bibliotek books = new Bibliotek();
+        books.bookList = new ArrayList<>();
 
-        ArrayList <Bog> bogliste = new ArrayList<>();
+        Bibliotek booksOne = new Bibliotek("Harry Potter","12343557881997");
+        books.addToBookList(booksOne);
+        Bibliotek booksTwo = new Bibliotek("Lord of the Rings", "987649981989");
+        books.addToBookList(booksTwo);
+        Bibliotek booksThree = new Bibliotek("H.C Andersen", "928463451789");
+        books.addToBookList(booksThree);
+        Bibliotek booksFour = new Bibliotek("The Little Mermaid", "12343557888327");
+        books.addToBookList(booksFour);
+
+        Scanner pickAISBN = new Scanner(System.in);
+        System.out.print("What ISBN are you looking for? ");
+        String chosenISBN = pickAISBN.next();
+
+        Bibliotek bookTest = new Bibliotek("", chosenISBN);
+        if (books.sameISBN(bookTest)){
+            System.out.println(bookTest.ISBN + " exist in the list");
+        }
+        else {
+            System.out.println(bookTest.ISBN + " does not exist in the list");
+        }
 
 
-        Bog b1 = new Bog("Harry", 1234355788, 1997);
-        Bog b2 = new Bog("lords of the Ring", 98764998,1989);
-        Bog b3 = new Bog("Tolken", 92846345, 1789);
-        Bog b4 = new Bog("Mermaid", 1234355788, 8327);
-
-        bogliste.add(b1);
-        bogliste.add(b2);
-        bogliste.add(b3);
-
-        System.out.println(bogliste);
-
-
-        /*Bog b2 = new Bog();
-        b2.titel = "Lord of the Rings";*/
 
     }
 
-    public static boolean bMethod (Bog currentBook, ArrayList bogliste) {
+    public Bibliotek(String name, String ISBN) {
+        this.name = name;
+        this.ISBN = ISBN;
+    }
 
-    return true; //Kompileringsfejl grundet manglede return statement
+    public Bibliotek(){
+    }
+
+    public void addToBookList(Bibliotek book){
+        bookList.add(book);
+    }
+
+    public boolean sameISBN(Bibliotek book) {
+        for (int i = 0; i < bookList.size(); i++) {
+            if (bookList.get(i).ISBN.equals(book.ISBN)) {
+                return true;
+            }
+        }
+        return false;
     }
 }
